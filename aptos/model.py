085b5cbaf836fb63a3316c4dec45b076f8ea53fd
Sergey Panchenko <sergey.panchenko@eos.com>
2019-08-01 15:46:05 +0300

    add batchnorm

--- aptos/model.py
+++ aptos/model.py
@@ -179,12 +179,12 @@ def model_keras(k):
 
         model = Sequential()
         model.add(ret)
-        # model.add(layers.BatchNormalization(axis=-1, momentum=0.99, epsilon=0.001, center=True, scale=True,
-        #                                     beta_initializer='zeros', gamma_initializer='ones',
-        #                                     moving_mean_initializer='zeros',
-        #                                     moving_variance_initializer='ones', beta_regularizer=None,
-        #                                     gamma_regularizer=None,
-        #                                     beta_constraint=None, gamma_constraint=None))
+        model.add(layers.BatchNormalization(axis=-1, momentum=0.99, epsilon=0.001, center=True, scale=True,
+                                            beta_initializer='zeros', gamma_initializer='ones',
+                                            moving_mean_initializer='zeros',
+                                            moving_variance_initializer='ones', beta_regularizer=None,
+                                            gamma_regularizer=None,
+                                            beta_constraint=None, gamma_constraint=None))
         model.add(layers.GlobalAveragePooling2D())
 
         # model.add(layers.Dropout(0.5))
