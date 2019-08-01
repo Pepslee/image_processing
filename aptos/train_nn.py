76362800c5c44d7908e62e8b163ba8f413870b24
Sergey Panchenko <sergey.panchenko@eos.com>
2019-08-01 13:18:52 +0300

    add start lr from params

--- aptos/train_nn.py
+++ aptos/train_nn.py
@@ -71,7 +71,7 @@ def main(csv_path, image_dir, ckpts_path, batch_size):
         test_generator = DataGenerator(test_df, '../resized_train_15_1024_1024', batch_size, 'test', '.jpg')
 
         model = model_keras(k)
-        optimizer_type = optimizer('Adam', 0.00005)
+        optimizer_type = optimizer('Adam', callbacks_params['start_lr'])
 
         model.compile(loss=loss, optimizer=optimizer_type, metrics=metrics)
         model.summary()
