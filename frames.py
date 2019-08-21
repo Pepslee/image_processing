a717f57dd04695ae52b11108bea49408900d9c9d
sergey.panchenko <sergey.panchenko@eosda.com>
2019-08-21 10:32:51 +0300

    add frames

--- frames.py
+++ frames.py
@@ -1,19 +1,21 @@
 import cv2
 import os
 
-DIR = "/home/serg/PycharmProjects/image_processing/video/camera_1/"
+filename = "/home/serg/PycharmProjects/image_processing/video/20190717_132436.mp4"
+DIR = '/home/serg/PycharmProjects/image_processing/video/camera_2'
 
 id = 0
-for filename in os.listdir(DIR):
-    if not filename.endswith(".mp4"):
-        continue
-    cap = cv2.VideoCapture(DIR + filename)
 
-    while True:
-        ok, frame = cap.read()
-        if not ok:
-            break
-        frame = cv2.resize(frame, (720, 360))
-        frame = frame[220:, :434]
-        cv2.imwrite(os.path.join(DIR, 'frames', str(id) + ".jpg"), frame)
-        id += 1
+cap = cv2.VideoCapture(filename, )
+
+if (cap.isOpened()== False):
+  print("Error opening video stream or file")
+
+while True:
+    ok, frame = cap.read()
+    if not ok:
+        break
+    frame = cv2.resize(frame, (720, 360))
+    frame = frame[220:, :434]
+    cv2.imwrite(os.path.join(DIR, str(id) + ".jpg"), frame)
+    id += 1
